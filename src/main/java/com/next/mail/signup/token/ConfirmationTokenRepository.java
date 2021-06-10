9f66a95f17e84396cb1bd6192da6564c0a32b46a
Steinny <charitymbisi@gmail.com>
2021-06-10 05:45:11 +0300

    token

---
+++ src/main/java/com/next/mail/signup/token/ConfirmationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.next.mail.signup.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+  Optional<ConfirmationToken> findByConfirmationToken(String token);
+}
---
+++ src/main/java/com/next/mail/signup/token/ConfirmationTokenService.java
@@ -0,0 +1,8 @@
+package com.next.mail.signup.token;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfirmationTokenService {
+
+}
